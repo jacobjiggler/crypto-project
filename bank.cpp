8f852fa2287eb4576fea24c3a25768f3bd78db92
Eric <eric@ubuntu.(none)>
2015-11-29 19:54:45 -0500

    Added userinfo class contining PIN and balance for each user account

--- bank.cpp
+++ bank.cpp
@@ -1,14 +1,16 @@
-#include<iostream>    //cout
-#include<stdio.h> //printf
-#include<string.h>    //strlen
-#include<string>  //string
+#include "userinfo.cpp"
+#include <iostream>    //cout
+#include <stdio.h> //printf
+#include <string.h>    //strlen
+#include <string>  //string
 #include <map>
-#include<cstdlib>
-//#include<sys/socket.h>    //socket
-//#include<arpa/inet.h> //inet_addr
-//#include<netdb.h> //hostent
+//#include <mutex>
+#include <cstdlib>
+//#include <sys/socket.h>    //socket
+//#include <arpa/inet.h> //inet_addr
+//#include <netdb.h> //hostent
 
-std::map<int, int> users;
+std::map<std::string, userInfo> users;
 
 int main(int argc , char *argv[])
 {
@@ -21,6 +23,6 @@ int main(int argc , char *argv[])
     return 1;
   }
 
-return 0;
+  return 0;
 
 }
